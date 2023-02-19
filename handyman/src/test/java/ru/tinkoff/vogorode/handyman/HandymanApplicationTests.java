4bb59d36fde6e9c70f56f23282ed5c0f40f0aac4
Aleksandr Bevz <as-bivz@yandex.ru>
2023-02-19 12:10:06 +0300

    Init commit Handyman service

---
+++ handyman/src/test/java/ru/tinkoff/vogorode/handyman/HandymanApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.tinkoff.vogorode.handyman;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HandymanApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
