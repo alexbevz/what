7c86f07ff61412a4bb853c728ca7d038903c4d96
Aleksandr Bevz <as-bivz@yandex.ru>
2023-02-19 12:11:09 +0300

    Init commit Landscape service

---
+++ landscape/src/test/java/ru/tinkoff/vogorode/landscape/LandscapeApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.tinkoff.vogorode.landscape;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LandscapeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
